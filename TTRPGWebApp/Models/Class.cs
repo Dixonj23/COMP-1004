7677c7332a5d38e16f46a552d3f710b0bd58ceef
Jack Dixon <jack.dixon@students.plymouth.ac.uk>
2023-11-23 16:17:06 +0000

    Learning

---
+++ TTRPGWebApp/Models/Class.cs
@@ -0,0 +1,14 @@
+﻿namespace TTRPGWebApp.Models
+{
+    public class Class
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int HitDie { get; set; }
+        public Class()
+        {
+           
+        }
+    }
+}
