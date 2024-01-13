b026c213b881cbf3c4507db776a99d90275c8c4d
Jack Dixon <jack.dixon@students.plymouth.ac.uk>
2024-01-13 16:07:57 +0000

    update

--- COMP1004 Project/Controllers/HomeController.cs
+++ COMP1004 Project/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ using System.Diagnostics;
 
 namespace COMP1004_Project.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
